939639bbece3c39a1408190aae739b7e6270646d
damianu <damian@datagentleman.io>
2025-02-01 23:14:52 +0100

    Parse Enode string

--- node/enode.go
+++ node/enode.go
@@ -3,6 +3,7 @@ package node
 import (
 	"crypto"
 	"crypto/ecdsa"
+	"fmt"
 	"net"
 
 	"github.com/ethereum/go-ethereum/p2p/enode"
@@ -17,4 +18,20 @@ func Enode(pub crypto.PublicKey, address string) (string, error) {
 
 	node := enode.NewV4(pub.(*ecdsa.PublicKey), addr.IP, addr.Port, 0)
 	return node.String(), nil
+}
+
+// Get public key and IP address from enode string.
+func ParseEnode(rawEnode string) (*ecdsa.PublicKey, string, error) {
+	node, err := enode.ParseV4(rawEnode)
+	if err != nil {
+		return nil, "", err
+	}
+
+	address, ok := node.TCPEndpoint()
+	if !ok {
+		// @CLEAN: At this point, the IP should have already been validated.
+		return nil, "",	fmt.Errorf("cannot parse node IP address: %s", node.IPAddr())
+	}
+
+	return node.Pubkey(), address.String(), nil
 }
