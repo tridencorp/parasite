2abe0dc03abf78290ee34481f1efdd7a38438d41
damianu <damian@datagentleman.io>
2025-02-02 19:27:32 +0100

    P2P reads are now returning Msg

--- p2p/peer.go
+++ p2p/peer.go
@@ -1,6 +1,8 @@
 package p2p
 
-import "github.com/ethereum/go-ethereum/p2p/rlpx"
+import (
+	"github.com/ethereum/go-ethereum/p2p/rlpx"
+)
 
 // Main struct handling P2P communication.
 type Peer struct {
@@ -12,8 +14,13 @@ func NewPeer(conn *rlpx.Conn) *Peer {
 	return &Peer{conn: conn}
 }
 
-// Reads data from a connected peer.
+// Reads message from a connected peer.
 // Blocks until data is available.
-func (p *Peer) Read() (code uint64, data []byte, wireSize int, err error) {
-	return p.conn.Read()
+func (p *Peer) Read() (Msg, error) {
+	code, data, size, err := p.conn.Read()
+	if err != nil {
+		return Msg{}, err
+	}
+
+	return Msg{Code: code, Size: uint32(size), Data: data}, err
 }
