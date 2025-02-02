2841e1211c1b79a4989c621587996b5fb0ecb11a
damianu <damian@datagentleman.io>
2025-02-03 00:19:44 +0100

    Handle handshakeMsg

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"parasite/key"
 	"parasite/node"
 	"parasite/p2p"
 )
 
-const nodePrv = "02f378c3758a72b90f0cb53e36f7873308ca1d7d450861939e163e615cc65dce"
+const nodePrv = "02f378c3758a72b90f0cb53e36f7873308ca1d7d450861939e163e615cĺc65dce"
 const nodeID  = "enode://e806157dfc5e11365210e09ad4af4fb129024de4a8c97c7a6c834daf9567200f9e8d03a769c1d53f13286a643e295b6f38073e90a1833c1e06ef23cc402cfecb@127.0.0.1:30303?discport=00"
 
 func main() {
@@ -21,18 +22,28 @@ func main() {
 		fmt.Print(err)
 	}
 
-	StartPeerReader(peer)
+	StartPeerReader(peer, srcPrv)
 }
 
 // Main peer reader responsible for handling all p2p messages.
-// @CLEAN: This could probably be moved to a different place.
-func StartPeerReader(peer *p2p.Peer) {
-	msg, err := peer.Read()
-	if err != nil {
-		fmt.Print(err)
-	}
+func StartPeerReader(peer *p2p.Peer, srcPrv *ecdsa.PrivateKey) {
+	for {
+		msg, err := peer.Read()
+		if err != nil {
+			fmt.Print(err)
+			break
+		}
 
-	fmt.Print(msg.Code)
-	fmt.Print(msg.Data)
-	fmt.Print(msg.Size)
+		switch msg.Code {
+		case p2p.HandshakeMsg:
+			err := p2p.HandleHandshake(msg, peer, &srcPrv.PublicKey)
+			if err != nil {
+				fmt.Print(err)
+			}
+
+		default:
+			fmt.Printf("Unsupported msg code: %d", msg.Code)
+			fmt.Printf(string(msg.Data))
+		}
+	}
 }
---
+++ p2p/message_handlers.go
@@ -0,0 +1,36 @@
+package p2p
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+// Parse handshake msg from remote peer and sends our response.
+func HandleHandshake(req Msg, peer *Peer, srcPub *ecdsa.PublicKey) error {
+	handshake := Handshake{}
+
+	// Remote handshake message
+	err := rlp.DecodeBytes(req.Data, &handshake)
+	if err != nil {
+		return err
+	}
+
+	// For now let's try to send the same handshake only changing the ID.
+	buf := bytes.Buffer{}
+
+	// First byte is only a prefix that indicates if the key is compressed. We can omit it.
+	handshake.ID = crypto.FromECDSAPub(srcPub)[1:]
+
+	err = rlp.Encode(&buf, handshake)
+	if err != nil {
+		return err
+	}
+
+	res := Msg{Code: HandshakeMsg, Size: uint32(buf.Len()), Data: buf.Bytes()}
+	peer.Send(res)
+
+	return nil
+}
