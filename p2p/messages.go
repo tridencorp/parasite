815339cd4dc1733258ed3de2dfc29e1cb730564f
damianu <damian@datagentleman.io>
2025-02-05 18:22:09 +0100

    BlockHeader and BlockHeaders structs

--- p2p/messages.go
+++ p2p/messages.go
@@ -1,6 +1,9 @@
 package p2p
 
 import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
@@ -9,7 +12,7 @@ import (
 
 const ETH = 68
 
-const BaseProtocolLen = 16
+const BaseProtocolSize = 16
 
 const (
 	// Base protocol msg codes: 0x00...0x10 (0-16)
@@ -21,13 +24,13 @@ const (
 
 	// Extended protocol msg codes: 0x10...0x1C (16-28)
 	StatusMsg                     = 0x10
-	NewBlockHashesMsg             = 0x11
-	TranasctionsMsg               = 0x12
+	NewBlockHashesMsg             = 0x11 // Not supported anymore
+	TransactionsMsg               = 0x12
 	GetBlockHeadersMsg            = 0x13
 	BlockHeadersMsg               = 0x14
 	GetBlockBodiesMsg             = 0x15
 	BlockBodiesMsg                = 0x16
-	NewBlockMsg                   = 0x17
+	NewBlockMsg                   = 0x17 // Not supported anymore
 	NewPooledTranasctionHashesMsg = 0x18
 	GetPooledTransactionsMsg      = 0x19
 	PooledTransactionsMsg         = 0x1A
@@ -49,4 +52,43 @@ type Handshake struct {
 
 	// Currently unused, but required for compatibility with ETH.
 	Rest []rlp.RawValue `rlp:"tail"`
+}
+
+// type GetBlocksPacket struct {
+// 	RequestId uint64
+// 	Headers []common.Hash
+// }
+
+type Bloom [256]byte
+type BlockNonce [8]byte
+
+type BlockHeader struct {
+	ParentHash  common.Hash
+	UncleHash   common.Hash
+	Coinbase    common.Address
+	Root        common.Hash
+	TxHash      common.Hash
+	ReceiptHash common.Hash
+	Bloom       Bloom
+	Difficulty  *big.Int
+	Number      *big.Int
+	GasLimit    uint64
+	GasUsed     uint64
+	Time        uint64
+	Extra       []byte
+	MixDigest   common.Hash
+	Nonce       BlockNonce
+
+	// optional
+	BaseFee           *big.Int      `rlp:"optional"`
+	WithdrawalsHash   *common.Hash  `rlp:"optional"`
+	BlobGasUsed       *uint64       `rlp:"optional"`
+	ExcessBlobGas     *uint64       `rlp:"optional"`
+	ParentBeaconRoot  *common.Hash  `rlp:"optional"`
+	RequestsHash      *common.Hash  `rlp:"optional"`
+}
+
+type BlockHeaders struct {
+	RequestId uint64
+	Headers []*BlockHeader
 }
