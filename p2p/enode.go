31807a88843a2713e75e068874c278d0f77cf7a6
damianu <damian@datagentleman.io>
2025-02-12 22:00:02 +0100

    Move Connect end Enode functions to p2p

---
+++ p2p/enode.go
@@ -0,0 +1,36 @@
+package p2p
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"net"
+
+	"github.com/ethereum/go-ethereum/p2p/enode"
+)
+
+// Return enode string. Address must be in "ip:port" format.
+func Enode(pub *ecdsa.PublicKey, address string) (string, error) {
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		return "", err
+	}
+
+	node := enode.NewV4(pub, addr.IP, addr.Port, 0)
+	return node.String(), nil
+}
+
+// Get public key and IP address from enode string.
+func ParseEnode(rawEnode string) (*ecdsa.PublicKey, string, error) {
+	node, err := enode.ParseV4(rawEnode)
+	if err != nil {
+		return nil, "", err
+	}
+
+	address, ok := node.TCPEndpoint()
+	if !ok {
+		// @CLEAN: At this point, the IP should have already been validated.
+		return nil, "",	fmt.Errorf("cannot parse node IP address: %s", node.IPAddr())
+	}
+
+	return node.Pubkey(), address.String(), nil
+}
---
+++ p2p/peer_connect.go
@@ -0,0 +1,119 @@
+package p2p
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"net"
+	"parasite/key"
+	"time"
+
+	"github.com/ethereum/go-ethereum/p2p/rlpx"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+type Handshake struct {
+	Version uint64
+	Name string
+
+	Caps []struct{ 
+		Name string
+		Version uint
+	}
+
+	ListenPort uint64
+	ID []byte
+
+	// Currently unused, but required for compatibility with ETH.
+	Rest []rlp.RawValue `rlp:"tail"`
+}
+
+// Connect to given node, perform handshake and exchange status msg.
+func Connect(enode string, srcPrv *ecdsa.PrivateKey) (*Peer, error) {
+	conn, err := initialHandshake(enode, srcPrv)
+	if err != nil {
+		return nil, err
+	}
+
+	err = handleHandshakeMessage(conn, srcPrv.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	err = exchangeStatus(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewPeer(conn), nil
+}
+
+func initialHandshake(enode string, prv *ecdsa.PrivateKey) (*rlpx.Conn, error)  {
+	dstPub, address, err := ParseEnode(enode)
+	if err != nil {
+		return nil, err
+	}
+
+	// Connect to node.
+	timeout := 1 * time.Second
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	// Perform initial handshake.
+	dst := rlpx.NewConn(conn, dstPub)
+	_, err = dst.Handshake(prv)
+	if err != nil {
+		return nil, err
+	}
+
+	return dst, nil
+}
+
+func handleHandshakeMessage(conn *rlpx.Conn, pub ecdsa.PublicKey) error {
+	handshake := Handshake{}
+
+	_, data, _, err := conn.Read()
+	if err != nil {
+		return fmt.Errorf("Handshake | error while reading from peer")
+	}
+
+	// Decode remote handshake message.
+	err = rlp.DecodeBytes(data, &handshake)
+	if err != nil {
+		return fmt.Errorf("Handshake | cannot decode bytes from peer")
+	}
+
+	// ID is basically our servers public key.
+	handshake.ID = key.PubToBytes(&pub)
+
+	// This will disable the snappy compression.
+	handshake.Version = 0
+
+	buf, err := rlp.EncodeToBytes(handshake)
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write(HandshakeMsg, buf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// @HACK: We just resend the same status that we got from remote peer.
+func exchangeStatus(dst *rlpx.Conn) error {
+	_, data, _, err := dst.Read()
+	if err != nil {
+		return err
+	}
+
+	_, err = dst.Write(StatusMsg, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
