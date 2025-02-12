ea283bfff1c3371191a714d07135cc2e433bfd98
damianu <damian@datagentleman.io>
2025-02-12 19:19:46 +0100

    Clean dispatcher and write some tests

--- server/dispatcher.go
+++ server/dispatcher.go
@@ -1,11 +1,8 @@
 package server
 
 import (
-	"fmt"
 	"parasite/log"
 	"parasite/p2p"
-
-	"github.com/ethereum/go-ethereum/rlp"
 )
 
 // Dispatching received messages to designated handlers.
@@ -16,7 +13,7 @@ type Dispatcher struct {
 	failure chan p2p.Msg
 }
 
-// Create new Dispatcher,
+// Create new Dispatcher.
 func NewDispatcher(peer *p2p.Peer, handler chan p2p.Msg, failure chan p2p.Msg) *Dispatcher {
 	return &Dispatcher{peer, handler, failure}
 }
@@ -24,98 +21,57 @@ func NewDispatcher(peer *p2p.Peer, handler chan p2p.Msg, failure chan p2p.Msg) *
 // Main dispatcher responsible for dispatching all incomming messages.
 // It uses 2 channels: one for normal message handling and another one 
 // for sending errors.
-func (dsp *Dispatcher) Dispatch(msg p2p.Msg) { 
-  // (2) PingMsg
+// Dispatcher is called by peer each time new message arrives.
+func (dispatcher *Dispatcher) Dispatch(msg p2p.Msg) { 
   if msg.Code == p2p.PingMsg {
-    log.Info("!!! Got Ping !!!")
-    dsp.peer.Send(p2p.NewMsg(p2p.PongMsg, []byte{}))
+    dispatcher.peer.Send(p2p.NewMsg(p2p.PongMsg, []byte{}))
     return
   }
 
-  // (19) GetBlockHeadersMsg
-  if msg.Code == p2p.GetBlockHeadersMsg {
-    log.Info("%d", msg.Code)
-    log.Info("%v", msg.Data)    
+	if msg.Code == p2p.BlockHeadersMsg {
+		dispatcher.handler <- msg
+		return
+	}
+
+	if msg.Code == p2p.DiscMsg {
+		dispatcher.handler <- msg
+		return
+	}
+
+	if msg.Code == p2p.NewPooledTransactionHashesMsg { 
+		dispatcher.handler <- msg
+		return
+	}
+
+	if msg.Code == p2p.TransactionsMsg {
+		dispatcher.handler <- msg
+		return
+	}
+
+	if msg.Code == p2p.BlockBodiesMsg { 
+		dispatcher.handler <- msg
+		return 
+	}
+
+	if msg.Code == p2p.ReceiptsMsg { 
+		dispatcher.handler <- msg
+		return 
+	}
+
+	if msg.Code == p2p.GetBlockHeadersMsg {
+		dispatcher.handler <- msg
     return
   }
 
-  // (20) BlockHeadersMsg
-  if msg.Code == p2p.BlockHeadersMsg {
-    log.Info("!!! Got headers !!!")
-
-    res, err := p2p.BlockHeadersRes(msg)
-    if err != nil {
-      log.Error("%v", err)
-    }
-
-    // Send msg with requsted headers to our handler.
-    reqMsg, exists := dsp.peer.RequestedMsgs[res.ReqId]
-    if exists {
-      reqMsg.Handler <- msg
-    }
-
-    return
-  }
-
-  // (1) DiscMsg
-  if msg.Code == p2p.DiscMsg {
-    log.Error("!!! DISCONECT FROM NODE !!!")
-
-    type DiscReason uint8
-    var disc []DiscReason
-
-    rlp.DecodeBytes(msg.Data, &disc)
-    log.Error("Disconnect from peer: %s", p2p.DiscReasons[disc[0]])
-    return
-  }
-
-  if msg.Code == p2p.NewPooledTransactionHashesMsg { 
-    log.Info("Request: %d : NewPooledTransactions", msg.Code)
-
-    pooledTx, err := p2p.PooledTransactions(msg)
-    if err != nil {
-      fmt.Println(err)
-    }
-    
-    fmt.Printf("%v", pooledTx)
-    return
-  }
-
-  if msg.Code == p2p.TransactionsMsg {
-    log.Info("Request: %d : p2p.TransactionsMsg", msg.Code)
-
-    txs, err := p2p.NewTransactions(msg)
-    if err != nil {
-      fmt.Println(err)
-    }
-
-    fmt.Printf("%v", txs)
-    return
-  }
-
-  // (21) GetBlockBodiesMsg
-  // We got request for block bodies.
   if msg.Code == p2p.GetBlockBodiesMsg {
-    log.Info("Request: %d : p2p.GetBlockBodiesMsg", msg.Code)
-
-    // Find the requested blocks and send them back to peer.
-    // For now we don't have any data so we send empty response.
-    blockBodies, _ := p2p.BlockBodiesRes(msg)
-
-    data, err := rlp.EncodeToBytes(blockBodies)
-    if err != nil {
-      log.Error(err.Error())
-    }
-
-    msg := p2p.NewMsg(p2p.GetBlockBodiesMsg, data)
-    fmt.Println(msg)
+		dispatcher.handler <- msg
     return
   }
-
-  // @TODO: Needs to be implemented
-  if msg.Code == p2p.BlockBodiesMsg { log.Error("Implement %d", p2p.BlockBodiesMsg)    ;return }
-  if msg.Code == p2p.GetReceiptsMsg { log.Error("Implement %d", p2p.GetReceiptsMsg)    ;return }
-  if msg.Code == p2p.ReceiptsMsg    { log.Error("Implement %d", p2p.ReceiptsMsg)       ;return }
+  
+	if msg.Code == p2p.GetReceiptsMsg { 
+		dispatcher.handler <- msg
+		return 
+	}
 
   // If we are here then we have unsupported message. 
   // Just print it for now.
---
+++ server/dispatcher_test.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"parasite/p2p"
+	"testing"
+)
+
+func TestMessageDispatch(t *testing.T) {
+	msg1 := p2p.NewMsg(p2p.BlockHeadersMsg,    []byte("BlockHeadersMsg"))
+  msg2 := p2p.NewMsg(p2p.DiscMsg,            []byte("DiscMsg"))     
+  msg3 := p2p.NewMsg(p2p.TransactionsMsg,    []byte("TransactionsMsg"))
+  msg4 := p2p.NewMsg(p2p.BlockBodiesMsg,     []byte("BlockBodiesMsg"))     
+  msg5 := p2p.NewMsg(p2p.ReceiptsMsg,        []byte("ReceiptsMsg"))        
+  msg6 := p2p.NewMsg(p2p.GetBlockHeadersMsg, []byte("GetBlockHeadersMsg"))
+  msg7 := p2p.NewMsg(p2p.GetBlockBodiesMsg,  []byte("GetBlockBodiesMsg"))
+  msg8 := p2p.NewMsg(p2p.GetReceiptsMsg,     []byte("GetReceiptsMsg"))
+  msg9 := p2p.NewMsg(p2p.NewPooledTransactionHashesMsg, []byte("NewPooledTransactionHashesMsg"))
+
+	tests := []struct {
+		name 	   string
+		message  p2p.Msg
+		expected p2p.Msg
+	}{
+		{ name: "BlockHeadersMsg",    message: msg1, expected: msg1},
+		{ name: "DiscMsg",            message: msg2, expected: msg2},
+		{ name: "TransactionsMsg",    message: msg3, expected: msg3},
+		{ name: "BlockBodiesMsg",     message: msg4, expected: msg4},
+		{ name: "ReceiptsMsg",        message: msg5, expected: msg5},
+		{ name: "GetBlockHeadersMsg", message: msg6, expected: msg6},
+		{ name: "GetBlockBodiesMsg",  message: msg7, expected: msg7},
+		{ name: "GetReceiptsMsg",     message: msg8, expected: msg8},
+		{ name: "NewPooledTransactionHashesMsg", message: msg9, expected: msg9},
+	}
+
+	handler := make(chan p2p.Msg, 1)
+	dsp := NewDispatcher(nil, handler, handler)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			dsp.Dispatch(test.message)
+			result := <- handler
+
+			if result.Code != test.expected.Code {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
+	}
+}
