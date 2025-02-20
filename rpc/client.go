6030a69ea6ee90005b97f5f14bdb66e3e32bb495
damianu <damian@datagentleman.io>
2025-02-20 12:35:18 +0100

    Clean rpc code

--- rpc/client.go
+++ rpc/client.go
@@ -1,125 +1,45 @@
 package rpc
 
 import (
-	"encoding/hex"
 	"fmt"
-	"math/big"
-	"strconv"
 )
 
-// eth_blockNumber
-//
 // Get the number of most recent block.
-func (node *Node) BlockNumber() (int64, error) {
-	res := ""
-
-	err := node.Send("eth_blockNumber", nil, &res)
-	if err != nil {
-		return -1, err
-	}
-
-	return strconv.ParseInt(res[2:], 16, 32)
+func (node *Node) BlockNumber(res any) error {
+	return node.Send("eth_blockNumber", res, nil)
 }
 
-// eth_getBalance
-// 
 // Return the balance of the account of given address.
-func (node *Node) GetBalance(address string) (int64, error) {
-	params := []any{address, "latest"}
-	res := ""
-
-	err := node.Send("eth_getBalance", params, &res)
-	if err != nil {
-		return -1, err
-	}
-
-	return strconv.ParseInt(res[2:], 16, 32)
+func (node *Node) GetBalance(res any, address string) error {
+	return node.Send("eth_getBalance", res, []any{address, "latest"})
 }
 
-// eth_gasPrice
-// 
 // Return current gas price.
-func (node *Node) GasPrice() (*big.Int, error) {
-	res := ""
-
-	err := node.Send("eth_gasPrice", nil, &res)
-	if err != nil {
-		return nil, err
-	}
-
-	price := new(big.Int)
-	_, ok := price.SetString(res[2:], 16); 
-	if !ok {
-		return nil, fmt.Errorf("Error converting hex string to big.Int")
-	}
-
-	return price, nil
+func (node *Node) GasPrice(res any) error {
+  return node.Send("eth_gasPrice", res, nil)
 }
 
-// eth_getCode
-//
 // Return code at a given address.
-func (node *Node) GetCode(address string) ([]byte, error) {
-	params := []any{address, "latest"}
-	res := ""
-
-	err := node.Send("eth_getCode", params, &res)
-	if err != nil {
-		return nil, err
-	}
-
-	bytecode, err := hex.DecodeString(res[2:])
-	return bytecode, nil
+func (node *Node) GetCode(res any, address string) error {
+  return  node.Send("eth_getCode", res, []any{address, "latest"})
 }
 
-// eth_getBlockByNumber
-// 
 // Return block by given number.
-func (node *Node) GetBlockByNumber(number uint32) (*Block, error) {
-	hex := fmt.Sprintf("0x%x", number)
-	params := []any{hex, true}
-	res := &Block{}
-
-	err := node.Send("eth_getBlockByNumber", params, res)
-	if err != nil {
-		return res, err
-	}
-
-	return res, nil
+func (node *Node) GetBlockByNumber(res any, number uint32) error {
+  hex := fmt.Sprintf("0x%x", number)
+	return node.Send("eth_getBlockByNumber", res, []any{hex, true})
 }
 
-// eth_getTransactionByHash
-// 
 // Return transaction by given hash.
-func (node *Node) GetTransactionByHash(hash string) (*Transaction, error) {
-	params := []any{hash}
-	res := &Transaction{}
-
-	err := node.Send("eth_getTransactionByHash", params, res)
-	if err != nil {
-		return res, err
-	}
-
-	return res, nil
+func (node *Node) GetTransactionByHash(res any, hash string) error {
+	return node.Send("eth_getTransactionByHash", res, []any{hash})
 }
 
-// eth_getTransactionReceipt
-// 
 // Return receipt for given transaction hash.
-func (node *Node) GetTransactionReceipt(hash string) (*Receipt, error) {
-	params := []any{hash}
-	res := &Receipt{}
-
-	err := node.Send("eth_getTransactionReceipt", params, res)
-	if err != nil {
-		return res, err
-	}
-
-	return res, nil
+func (node *Node) GetTransactionReceipt(res any, hash string) error {
+  return node.Send("eth_getTransactionReceipt", res, []any{hash})
 }
 
-// eth_sendRawTransaction
-// 
 // Send signed transaction (or contract) to ethereum network.
 func (node *Node) SendRawTransaction(res any, hex string) error {
   return node.Send("eth_sendRawTransaction", res, []any{hex})
--- rpc/client_test.go
+++ rpc/client_test.go
@@ -2,6 +2,8 @@ package rpc
 
 import (
 	"testing"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 const localAddress   = "http://127.0.0.1:8545"
@@ -11,58 +13,64 @@ const localTx        = "0x3dac2080b4c423029fcc9c916bc430cde441badfe736fc6d1fe932
 const rawTx          = "0x3dac2080b4c423029fcc9c916bc430cde441badfe736fc6d1fe9325348af80fd"
 
 func TestBlockNumber(t *testing.T) {
-	node := NewNode(localAddress)
-	res, err := node.BlockNumber()
+  node := NewNode(localAddress)
+  res := hexutil.Uint64(0)
+  err := node.BlockNumber(&res)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
-	if res <= 0   { t.Errorf("Expected response to be > 0, got %d", res) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if res <= 0   { t.Errorf("Expected response to be > 0, got %d", res) }
 }
 
 func TestBalance(t *testing.T) {
-	node := NewNode(localAddress)
-	_, err := node.GetBalance(accountAddress)
+  node := NewNode(localAddress)
+  res := ""
+  err := node.GetBalance(&res, accountAddress)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
 }
 
 func TestGasPrice(t *testing.T) {
-	node := NewNode(localAddress)
-	_, err := node.GasPrice()
+  node := NewNode(localAddress)
+  res := ""
+  err := node.GasPrice(&res)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
 }
 
 func TestGetCode(t *testing.T) {
-	node := NewNode(localAddress)
-	_, err := node.GetCode(accountAddress)
+  node := NewNode(localAddress)
+  res := ""
+  err := node.GetCode(&res, accountAddress)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
 }
 
 func TestGetBlockByNumber(t *testing.T) {
-	node := NewNode(localAddress)
-	_, err := node.GetBlockByNumber(localBlock)
+  node := NewNode(localAddress)
+  res := &Block{}
+  err := node.GetBlockByNumber(res, localBlock)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
 }
 
 func TestGetTransactionByHash(t *testing.T) {
-	node := NewNode(localAddress)
-	_, err := node.GetTransactionByHash(localTx)
+  node := NewNode(localAddress)
+  res := &Transaction{}
+  err := node.GetTransactionByHash(res, localTx)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
 }
 
 func TestGetTransactionReceipt(t *testing.T) {
-	node := NewNode(localAddress)
-	_, err := node.GetTransactionReceipt(localTx)
+  node := NewNode(localAddress)
+  res := &Receipt{}
+  err := node.GetTransactionReceipt(res, localTx)
 
-	if err != nil { t.Errorf("Expected no errors, got %s", err) }
+  if err != nil { t.Errorf("Expected no errors, got %s", err) }
 }
 
 func TestSendRawTransaction(t *testing.T) {
   node := NewNode(localAddress)
-
   res := ""
   err := node.SendRawTransaction(&res, rawTx)
 
