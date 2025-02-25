a1e103a03a7efebcf44d63ec8f69caa2b6daa671
damianu <damian@datagentleman.io>
2025-02-25 12:45:14 +0100

    Add our main transaction type

---
+++ db/tx.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// This is our main transaction type. It will be used
+// in all internal operations.
+// All ethereum transactions will be mapped to it.
+type Tx struct {
+	Type              uint8
+	From              *common.Address
+	To                *common.Address
+	Value             *big.Int
+	Nonce             uint64
+	Hash              common.Hash
+	ChainID           *big.Int
+	Status            uint64
+	BlockNumber       *big.Int
+
+	GasUsed           uint64
+	EffectiveGasPrice big.Int
+	CumulativeGasUsed uint64
+	Gas               uint64
+	GasTipCap         *big.Int
+	GasFeeCap         *big.Int
+	V, R, S           *big.Int
+
+	Data []byte
+}
