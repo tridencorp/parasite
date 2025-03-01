3542e9a3010149c04b09a0c1f8f3654ea7d7fb28
damianu <damian@datagentleman.io>
2025-03-01 19:33:37 +0100

    Return N number of peers in tests

--- fetchers/fetcher_test.go
+++ fetchers/fetcher_test.go
@@ -2,31 +2,13 @@ package fetchers
 
 import (
 	"fmt"
-	"parasite/p2p"
 	"parasite/test"
 	"testing"
-
-	"github.com/ethereum/go-ethereum/rlp"
 )
+
 
 func TestFetchBlockHeaders(t *testing.T) {
 	// Test with valid response from all peers.
-	peers := []*test.Peer{}
-	fetcher := HeaderFetcher(3, 3, peers)
-
-	headers := test.Headers(1)
-	raw, _ := rlp.EncodeToBytes(headers)
-
-	// Simulate valid response from peers.
-	go func(res chan *p2p.Msg) {
-		for i:=0; i < 3; i++ {
-			res <- p2p.NewMsg(1, raw)
-		}
-	}(fetcher.PeerRes)
-	
-	fetcher.FetchBlockHeaders(uint64(0))
-	msg := <- fetcher.HandlerRes
-	res := msg.Payload.(*p2p.Msg)
-	
-	fmt.Println(res)
+	peers := test.Peers(3)
+	fmt.Println(peers)
 }
--- test/peer.go
+++ test/peer.go
@@ -10,6 +10,12 @@ type Peer struct {
 	Failure  chan *p2p.Msg
 }
 
-func (p *Peer) Send(msg *p2p.Msg) {
+func (p *Peer) Send(msg *p2p.Msg) {}
 
+// Return N number of peers.
+func Peers(num int) (peers []*Peer) {
+	for i:=0; i<num; i++ {
+		peers = append(peers, &Peer{})
+	}
+	return peers
 }
