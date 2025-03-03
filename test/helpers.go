2d3051777164b7472cca8871e0dc39d86d1ab465
damianu <damian@datagentleman.io>
2025-03-03 01:01:28 +0100

    Add some test helpers

--- test/helpers.go
+++ test/helpers.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"math/big"
 	"parasite/p2p"
+
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 func RandInt(max int) *big.Int {
@@ -16,12 +18,21 @@ func Headers(n int) []*p2p.BlockHeader {
 	headers := []*p2p.BlockHeader{}
 
 	for i:=0; i<n; i++ {
-		header := &p2p.BlockHeader{
+		header := &p2p.BlockHeader{ 
 			Number: RandInt(100_000_000),
 		}
-
 		headers = append(headers, header)
 	}
 
 	return headers
+}
+
+// Create p2p headers response message.
+func HeadersResponseMsg(code, num int) (*p2p.Msg, []*p2p.BlockHeader) {
+	headers := Headers(num)
+	data    := []any{uint64(code), headers}
+	req, _  := rlp.EncodeToBytes(data)
+	msg     := p2p.NewMsg(1, req)
+	
+	return msg, headers
 }
--- test/peer.go
+++ test/peer.go
@@ -8,14 +8,19 @@ import (
 type Peer struct {
 	Handler  chan *p2p.Msg
 	Failure  chan *p2p.Msg
+	Response *p2p.Msg
 }
 
-func (p *Peer) Send(msg *p2p.Msg) {}
+// For testing purpose we are immediately sending back
+// response.
+func (p *Peer) Send(msg *p2p.Msg) {
+	p.Handler <- p.Response
+}
 
 // Return N number of peers.
-func Peers(num int) (peers []*Peer) {
+func Peers(num int, res *p2p.Msg, handler chan *p2p.Msg) (peers []*Peer) {
 	for i:=0; i<num; i++ {
-		peers = append(peers, &Peer{})
+		peers = append(peers, &Peer{Response: res, Handler: handler})
 	}
 	return peers
 }
