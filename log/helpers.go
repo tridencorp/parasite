769f112db3170a6de5ad2faefd6124db170045c9
damianu <damian@datagentleman.io>
2025-03-03 15:04:43 +0100

    Format log for non basic types

--- log/helpers.go
+++ log/helpers.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -13,23 +14,40 @@ import (
 // If last element is not a tuple (single element), display
 // it based on it's type - without any resizing.
 func Format(args ...any) string {
-  reset  := "\033[0m"
-  format := ""
+	reset  := "\033[0m"
+	format := ""
 
-  for i:=0; i < len(args); i+=3 {
-    size  := "%-" + strconv.Itoa(args[i].(int)) + ftype()
-    color := Colors[args[i+1].(int)]
-    data  := args[i+2]
+	for i:=0; i < len(args); i+=3 {
+		data  := args[i+2]
+		kind := reflect.TypeOf(data).Kind()
 
-    format += fmt.Sprintf(color + size + reset, data)
-  }
+		size  := "%-" + strconv.Itoa(args[i].(int)) + ftype(kind)
+		color := Colors[args[i+1].(int)]
 
-  return format
+		if kind == reflect.Slice || kind  == reflect.Array || kind == reflect.Struct {
+			symbol := "%" + ftype(kind)
+			format += fmt.Sprintf(color + symbol + reset, data)
+			continue
+		}
+
+		format += fmt.Sprintf(color + size + reset, data)
+	}
+
+	return format
 }
 
 // Return fmt format symbol based on type.
-func ftype() string {
-  return "s"
+func ftype(kind reflect.Kind) string {
+	switch kind {
+		case reflect.String:
+			return "s"
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return "d"
+		case reflect.Float32, reflect.Float64:
+			return "f"
+		default:
+			return "v"
+	}
 }
 
 // Formatted error log.
